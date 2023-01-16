ef5a2d78a33fc6e6ac8a278738c8ceb8e73733b8
Tomás Santiago <thom.sgonzalez@gmail.com>
2023-01-15 18:31:54 -0600

    More data was added to the api response

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,19 +1,32 @@
-
 class InmuebleSerializer(object):
     """
     Clase base para serializar datos
     """
+
     _data = []
+
     def __init__(self, data):
         self.serializer(data)
 
     def serializer(self, data):
+        """
+        Recorrer datos y agrega en la lista
+        """
         for row in data:
-            self._data.append({
-                'id': row.id,
-                'description': row.description
-            })
+            self._data.append(
+                {
+                    "id": row.id,
+                    "address": row.address,
+                    "city": row.city,
+                    "price": row.price,
+                    "status": row.name,
+                    "description": row.description,
+                }
+            )
 
     @property
     def data(self):
+        """
+        Propiedad de la clase para acceder a self._data
+        """
         return self._data
